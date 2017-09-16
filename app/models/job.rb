89d873e5cecaa23f5f1f9067f48df7ddce4226fb
Frans Selles <frans.selles@gmail.com>
2017-09-16 11:47:53 +0200

    Added bootstrap gem and basic styling

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
 	has_many :tasks, dependent: :destroy
   accepts_nested_attributes_for :tasks
+  validates :contactperson, :title, :article_number, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :job
+  validates :description, presence: true
 end
